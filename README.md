dd33bee04d5e63a4c0cab4d9e175d358e05016bf
user <silanila5@gmail.com>
2020-11-06 20:31:44 +0200

    lastpbly

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# Flask website intro
-
-```
-pip install flask # tipiski, ja mums vajag vienu biblioteku
-pip install flask --user # tikai tad ja nav tiesibu un pirmais izlido ar error
-pip install -r requirments.txt # parasti izmantojam so
-```
-
-/about
-/contact
+#Chat
--- main.py
+++ main.py
@@ -5,45 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/getParam', methods = ['GET'])
-def getParam():
-    #print(request.args.get('vards'))
-    return f"It Works! {request.args.get('vards')}"
-
-
-@app.route('/postMsg', methods = ['POST'])
-def postMsg():
-    #print(request.json)
-    data = request.json
-    return f"This is post! {data['vards']}"
-
-@app.route('/info', methods = ['GET', 'POST'])
-def info():
-    if request.method == 'GET':
-        if request.args.get('vards'):
-            return f"Hello, {request.args.get('vards')}!"
-        else:
-            return "Hello, stranger!"
-
-    elif request.method == 'POST':
-        if request.content_type == 'application/json':
-            dati = request.json
-            return dati
-        else:
-            return f"Wrong content type: {request.content_type}"
-    else:
-        return "Unknow request method"
-
+    return render_template('chat.html')
 
 @app.route('/chat', methods = ['GET', 'POST'])
 def chat():
